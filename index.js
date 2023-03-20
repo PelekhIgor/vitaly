a99e0bbd3bcd153b821c5758527ab903f415442b
PelekhIgor <igorpelekh0112@gmail.com>
2023-03-20 23:18:53 +0200

    add slaid

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+$(window).on('load resize', function() {
+    if ($(window).width() < 795) {
+        $('#items:not(.slick-initialized)').slick({
+            infinite: true,
+            dots: true,
+            speed: 100,
+            slidesToShow: 1
+        });
+    } else {
+        $("#items.slick-initialized").slick("unslick");
+    }
+});
+
 const popupEl = document.querySelector('.popup')
 const cancelEl = document.querySelector('.close_popup')
 const btnEl = document.querySelector('.fake_question')
@@ -14,6 +27,10 @@ const rightPowerEl = document.getElementById('rightEye_Sphere')
 const leftPowerEl = document.getElementById('leftEye_Sphere')
 const rightBcEl = document.getElementById('rightEye_BaseCurve')
 const leftBcEl = document.getElementById('leftEye_BaseCurve')
+
+const costEl = document.getElementById('data_cost')
+const discountEl = document.getElementById('data_discount')
+const totalEl = document.getElementById('data_total')
 
 
 //About checked both eyes
@@ -41,6 +58,9 @@ labelTwelveEl.addEventListener('click', function (){
     labelThreeEl.classList.remove('active')
     boxLeftEl.value=4
     boxRightEl.value=4
+    costEl.innerHTML = '391.44 $'
+    discountEl.innerHTML = '56.8 $'
+    totalEl.innerHTML = '334.64 $'
 })
 labelSixEl.addEventListener('click', function (){
     labelTwelveEl.classList.remove('active')
@@ -48,6 +68,9 @@ labelSixEl.addEventListener('click', function (){
     labelThreeEl.classList.remove('active')
     boxLeftEl.value=2
     boxRightEl.value=2
+    costEl.innerHTML = '195.72 $'
+    discountEl.innerHTML = '25 $'
+    totalEl.innerHTML = '170.72 $'
 })
 labelThreeEl.addEventListener('click', function (){
     labelTwelveEl.classList.remove('active')
@@ -55,6 +78,9 @@ labelThreeEl.addEventListener('click', function (){
     labelThreeEl.classList.add('active')
     boxLeftEl.value=1
     boxRightEl.value=1
+    costEl.innerHTML = '97.86 $'
+    discountEl.innerHTML = '0 $'
+    totalEl.innerHTML = '97.86 $'
 })
 labelEightEl.addEventListener('click', function (){
     labelTwelveEl.classList.remove('active')
@@ -62,6 +88,9 @@ labelEightEl.addEventListener('click', function (){
     labelThreeEl.classList.remove('active')
     boxLeftEl.value=8
     boxRightEl.value=8
+    costEl.innerHTML = '782.88 $'
+    discountEl.innerHTML = '112 $'
+    totalEl.innerHTML = '670.88 $'
 })
 
 
@@ -86,3 +115,4 @@ function popupOpen(popupEl){
 function popupClose(popupActive){
     popupActive.classList.remove('open')
 }
+
